2b7809122ab40791973f19135e947daa8d694446
Jony-Mia <ajonymia321@gmail.com>
2026-07-02 14:01:43 +0600

    update added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dns = require("dns/promises");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config()
 app.use(cors());
-// app.use(express.urlencoded())
+
 app.use(express.json());
 app.use(cors({ origin: "*" }))
 const client = new MongoClient(uri, {
@@ -19,19 +19,20 @@ const client = new MongoClient(uri, {
         strict: true
     }
 })
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send({
-        message:"Everything is ok",
-        status:true
+        message: "Everything is ok",
+        status: true
     })
 })
 async function run() {
     try {
-        
+
         const startupForge = client.db("startup_forge");
         const getOpportunities = startupForge.collection("opportunities")
+        const startupList = startupForge.collection("startup_collection")
         app.get("/opportunities", async (req, res) => {
-            
+
             const data = await getOpportunities.find().toArray()
             res.send(data)
         });
@@ -40,7 +41,11 @@ async function run() {
             const newOpportunites = await getOpportunities.insertOne(newData);
 
             res.send(newData);
-        })
+        });
+        app.get("/getStartup", async (req, res) => {
+            let startupData = await startupList.find().toArray()
+            res.send(startupData)
+        });
         app.delete(`/deleteOpportunities/:id`, async (req, res) => {
             let id = req.params.id;
             const result = await getOpportunities.deleteOne({ _id: new ObjectId(id) })
@@ -48,11 +53,45 @@ async function run() {
                 console.log("Successfully deleted one document.");
             } else {
                 console.log("No documents matched the query. Deleted 0 documents.");
-            }                         
+            }
             res.send(result);
+        });
+
+        app.post("/createStartup", async (req, res) => {
+            let body = req.body;
+
+            let startupData = body[0];
+            startupData.image = body[1];
+
+            const createStartup = await startupList.insertOne(startupData);
+
+            res.send(body)
+        });
+
+        app.get("/singleStartup/:id", async (req, res) => {
+            let id = await req.params.id;
+            let singleData = await startupList.findOne({ _id: new ObjectId(id) })
+            console.log(id);
+
+            console.log(singleData);
+            res.send(singleData);
+        })
+        app.patch("/editStartup/:id", async (req, res) => {
+            let updated = await req.body;
+            let id = await req.params.id;
+            let filter = {
+                _id: new ObjectId(id)
+            }
+            let bodyData = updated[0];
+            bodyData.image = updated[1]
+            let updateStartup = await startupList.updateOne(filter, {
+                $set: bodyData
+            })
+            console.log(bodyData);
+            res.send(bodyData)
         })
     } catch (error) {
-       await client.close();
+        await client.close();
         console.log(error);
     }
 }
